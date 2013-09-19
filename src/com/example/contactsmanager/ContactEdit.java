0cdc882796721f75621d92d77dee03c66d333fb1
joshofreeness <joshofreeness@gmail.com>
2013-09-19 16:43:23 +1200

    Added buttons in contactview to press to call/text/email

--- src/com/example/contactsmanager/ContactEdit.java
+++ src/com/example/contactsmanager/ContactEdit.java
@@ -55,8 +55,7 @@ public class ContactEdit extends Activity{
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.contact_edit, menu); 
-        
-        //TODO: make buttons work
+
         return true;
     }
     
--- src/com/example/contactsmanager/ContactView.java
+++ src/com/example/contactsmanager/ContactView.java
@@ -6,9 +6,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,17 +24,8 @@ public class ContactView extends Activity{
         Intent myIntent = getIntent();
         setContentView(R.layout.contact_view);
         name = myIntent.getStringExtra("name");
-        ContactList cList = ContactList.getInstance();
-    	ArrayList<Contact> values = cList.getList();
-    	//TODO: CAN LEAD TO NULL POINTER
-    	Contact contact = null;
-    	for (int i=0; i<values.size(); i++){
-    		if (values.get(i).getFullName().equals(name)){
-    			contact = values.get(i);
-    		}
-    	}
-    	//Set fields
-    	String[] details = contact.getDetails();
+        
+    	String[] details = getDetails();
     	
         TextView firstName = (TextView)findViewById(R.id.contact_first_name);
         firstName.setText(details[0]);
@@ -51,6 +45,49 @@ public class ContactView extends Activity{
         //TODO:Setup DOB details[7]
         //TODO: change when setup images details[8]
         image.setImageResource(R.drawable.ic_launcher);
+        
+        
+        ImageButton callMob = (ImageButton)findViewById(R.id.call_mobile_image);
+        callMob.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        		callNumber(1);
+        	}
+        	});
+        ImageButton callWork = (ImageButton)findViewById(R.id.call_work_image);
+        callWork.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        		callNumber(3);
+        	}
+        	});
+        ImageButton callHome = (ImageButton)findViewById(R.id.call_home_image);
+        callHome.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        		callNumber(2);
+        	}
+        	});
+        ImageButton textMob = (ImageButton)findViewById(R.id.text_mobile_image);
+        textMob.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        		//Intent to text mobile
+        		String[] details = getDetails();
+        		Uri smsUri = Uri.parse("sms:"+ details[4]);
+        		Intent intent = new Intent(Intent.ACTION_VIEW, smsUri);
+        		startActivity(intent);
+        		
+        	}
+        	});
+        
+        ImageButton sendEmail = (ImageButton)findViewById(R.id.email_image);
+        sendEmail.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v){
+        		//Intent to text mobile
+        		String[] details = getDetails();
+        		Uri emailUri = Uri.parse("mailto:"+ details[5]);
+        		Intent intent = new Intent(Intent.ACTION_VIEW, emailUri);
+        		startActivity(intent);
+        		
+        	}
+        	});
     }
     
     @Override
@@ -95,6 +132,39 @@ public class ContactView extends Activity{
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    
+    private void callNumber(int number){
+    	String[]details = getDetails();
+     	String phNo;
+     	
+     	if (number == 1){
+     		phNo=details[4];
+     	}else if(number == 2){
+     		phNo=details[2];
+     	}else{
+     		phNo=details[3];
+     	}
+     	
+     	Intent intent = new Intent(Intent.ACTION_DIAL);
+     	intent.setData(Uri.parse("tel:" + phNo )); 
+     	startActivity(intent); 
+    	
+    }
+    
+    public String[] getDetails(){
+    	 ContactList cList = ContactList.getInstance();
+      	ArrayList<Contact> values = cList.getList();
+      	//TODO: CAN LEAD TO NULL POINTER
+      	Contact contact = null;
+      	for (int i=0; i<values.size(); i++){
+      		if (values.get(i).getFullName().equals(name)){
+      			contact = values.get(i);
+      		}
+      	}
+      	//Set fields
+      	return contact.getDetails();
+    	
     }
 	
 	
